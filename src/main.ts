22d9c43713441858570e558abbc10e0675d76d64
Juhyeong Ko <dury.ko@gmail.com>
2023-02-12 00:56:33 +0900

    feat: support config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = getPort(configService);
+
+  await app.listen(port);
+
+  return port;
 }
-bootstrap();
+
+function getPort(configService: ConfigService) {
+  return configService.get<number>('PORT');
+}
+
+bootstrap().then((port) => {
+  const logger = new Logger('bootstrap');
+
+  logger.log(`Server listening on PORT ${port}`);
+});
